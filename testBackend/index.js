f33ce814aad8480cb945e62d45aedf17e813367c
Salim Khan <131733026+salimkhandev2@users.noreply.github.com>
2024-10-11 16:24:09 +0500

    Update index.js

--- testBackend/index.js
+++ testBackend/index.js
@@ -20,7 +20,9 @@ app.post('/set-cookie', (req, res) => {
  
    let value="SalimKhan"; // Get the value from the request body
     res.cookie('myCookie', value, {
-        httpOnly: true, // Cookie cannot be accessed via JavaScript
+        httpOnly: true,
+          domain: 'cookies-testing-frontend.vercel.app',  // Exact domain of your frontend
+// Cookie cannot be accessed via JavaScript
         secure:true, // Use secure cookies in production
         sameSite: 'None', // CSRF protection
         maxAge: 3600000 // 1 hour
